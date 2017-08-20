1a847970d7d4c1075992312dadca8f9b0f59c2bd
Haruyuki Ichino <mail@icchi.me>
2017-08-20 15:37:03 +0900

    Improve: 使いやすいように改良

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 # keras_to_tensorflow
+kerasで出力したmodelファイル(.h5)をtensorflowのmodel形式(.pb)に変換するスクリプト
+
+[amir-abdi](https://github.com/amir-abdi)さんが公開されている.ipyenvコードを.pyに変換してから改良。  
+<https://github.com/amir-abdi/keras_to_tensorflow>
+
+* keras modelをコマンドライン引数から選択
+* 出力ファイル名は入力ファイルを元に定義
+
+
+## 使い方
+**kerasとtensorflowが動く環境を用意**
+
+```bash
+pip install keras
+pip install tensorflow
+```
+
+**以下のようなディレクトリ構造に**
+> 分類器ディレクトリ(以下から取得)
+> https://github.com/opencv/opencv/blob/master/data/haarcascades/
+> https://github.com/opencv/opencv_contrib/blob/master/modules/face/data/cascades>/
+
+```
+.
+├── README.md
+├── keras_model
+│   └── keras_model.h5
+├── keras_to_tensorflow.py
+└── tensorflow_model (存在しない場合は自動生成)
+```
+
+**実行**
+```bash
+# sample
+python keras_to_tensorflow.py keras_model/keras_model.h5
+```
+
+
+# Original README
+## keras_to_tensorflow
 General code to convert a trained keras model into an inference tensorflow model
 
 The notebook ```keras_to_tensorflow```, is a sample code which loads a trained keras model, freezes the nodes (converts all tensorflow variables to tensorflow constants), and saves the inference graph and weights into a protobuf file (.pb). This file can then be used to deploy the trained model for inference. During freezing, other nodes of the network, which do not contribute  the tensor that would contain the output predictions, are prunned. This results in a smaller, optimized network.
--- keras_to_tensorflow.py
+++ keras_to_tensorflow.py
@@ -1,46 +1,69 @@
-# Set parameters
-input_fld = 'input_fld_path'
-weight_file = 'kerasmodel_file_name located inside input_fld'
-num_output = 1
-write_graph_def_ascii_flag = True
-prefix_output_node_names_of_final_network = 'output_node'
-output_graph_name = 'constant_graph_weights.pb'
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+__doc__
+General code to convert a trained keras model into an inference tensorflow model.
+"""
+
+__author__ = "Haruyuki Ichino"
+__version__ = "1.0"
+__date__ = "2017/08/20"
+
+print(__doc__)
 
 
-# Initialize
+import sys
+
 from keras.models import load_model
 import tensorflow as tf
 import os
 import os.path as osp
 from keras import backend as K
 
-output_fld = input_fld + 'tensorflow_model/'
-if not os.path.isdir(output_fld):
-    os.mkdir(output_fld)
-weight_file_path = osp.join(input_fld, weight_file)
+from tensorflow.python.framework import graph_util
+from tensorflow.python.framework import graph_io
+
+
+# Set parameters
+if len(sys.argv) != 2:
+    print("Usage: python keras_to_tensorflow.py [keras model file path]")
+    sys.exit(1)
+keras_model_path = sys.argv[1]
+num_output = 1
+write_graph_def_ascii_flag = True
+prefix_output_node_names_of_final_network = 'output_node'
+keras_model_name = keras_model_path.split("/")[-1].split(".")[0]
+tensorflow_graph_name = keras_model_name + '.pb'
+
+# 出力ディレクトリの準備
+output_dir = './tensorflow_model/'
+if not os.path.isdir(output_dir):
+    os.mkdir(output_dir)
 
 
 # Load keras model and rename output
 K.set_learning_phase(0)
-net_model = load_model(weight_file_path)
+keras_model = load_model(keras_model_path)
 
 pred = [None]*num_output
 pred_node_names = [None]*num_output
 for i in range(num_output):
     pred_node_names[i] = prefix_output_node_names_of_final_network+str(i)
-    pred[i] = tf.identity(net_model.output[i], name=pred_node_names[i])
-print('output nodes names are: ', pred_node_names)
+    pred[i] = tf.identity(keras_model.output[i], name=pred_node_names[i])
+print('Output nodes names: ', pred_node_names)
+
 
 # [optional] write graph definition in ascii
 sess = K.get_session()
 if write_graph_def_ascii_flag:
-    f = 'only_the_graph_def.pb.ascii'
-    tf.train.write_graph(sess.graph.as_graph_def(), output_fld, f, as_text=True)
-    print('saved the graph definition in ascii format at: ', osp.join(output_fld, f))
+    f = tensorflow_graph_name + '.ascii'
+    tf.train.write_graph(sess.graph.as_graph_def(), output_dir, f, as_text=True)
+    print('Saved the graph definition: ', osp.join(output_dir, f))
+
 
 # convert variables to constants and save
-from tensorflow.python.framework import graph_util
-from tensorflow.python.framework import graph_io
 constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)
-graph_io.write_graph(constant_graph, output_fld, output_graph_name, as_text=False)
-print('saved the constant graph (ready for inference) at: ', osp.join(output_fld, output_graph_name))
+graph_io.write_graph(constant_graph, output_dir, tensorflow_graph_name, as_text=False)
+
+print('Saved the TensorFlow graph: ', osp.join(output_dir, tensorflow_graph_name))
